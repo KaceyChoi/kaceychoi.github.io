2d8742524e6e24c375ae0ef3229b1c1ee8af9666
KaceyChoi <131378522+KaceyChoi@users.noreply.github.com>
2023-05-31 21:32:50 -0400

    Add files via upload

---
+++ SE Bootcamp/may30.js
@@ -0,0 +1,137 @@
+alert("Hello JS");
+console.log('May30');
+
+
+let userInput=prompt('What would you like to order?');
+
+
+
+// if(typeof String(userInput)==='string')
+//   { 
+//     console.log('Ordering Tea..')
+//         unfinished...
+//   }
+//   else if(userInput==='Cake'){
+    
+//   }
+// else
+// {
+//     console.log(' Input..')
+
+   
+// }
+
+// different structure of if esle statement
+// if(userInput==='Cake' || )
+//   { 
+//     console.log('Ordering Tea..')
+//         unfinished...
+//   }
+//   else if(userInput==='Cake'){
+    
+//   }
+// else
+// {
+//   if (){
+
+//   }
+//   else if (){
+
+
+//   }
+//   else if(){
+
+//   }
+//   else{
+
+//   }
+
+
+// }
+
+let balanceUser=0;
+
+"falsy value: 0, '', NaN, null, undefined"
+   
+if(balanceUser) {
+  console.log('Track1');
+}
+else{console.log('Track 2')}
+
+
+// A=1<2
+// B=111=='111'
+// C=!'Peter'
+// Practice:
+// 3+ walk in clients 
+
+
+// let userName='PeterSmith';
+// let phoneNumbe=1212828273;
+// let addresss='101 NEW YORK'
+
+
+// petName1
+petSpecies='dog';
+// petAge1
+
+// if(){
+
+// }
+// else{
+
+// }
+
+// dogNum=0
+// petSpecies==='dog'?console.log(`here is a dog,total number of dogs: ${dogNum++}`):console.log('Not a dog'); 
+// petSpecies='dog';
+// petSpecies==='dog'?console.log(`here is a dog,total number of dogs: ${dogNum++}`):console.log('Not a dog'); 
+
+// const summary1=`${userName}\'s pet ${petName} is a ${petAge} old  ${petSpecies}`
+// userName=''
+
+// userName2='LucasDoe'
+// phoneNumber2
+// addresss2
+
+// petName2
+// petSpecies2
+// petAge2
+
+// const summary2=`${userName}\'s pet ${petName} is a ${petAge} old  ${petSpecies}`
+
+// const summaryInfo=`${userName1}\'s pet ${petName1} is a ${petAge1} old  ${petSpecies1}`
+
+// const summaryInfo2=`${summary1},${summary2}};
+// result:
+// 'PeterSmith\'s pet Rex is a 2 years old dog,\n 
+// Lucas Doe's pet Miao is a 1 year old CAT, ....
+// Total clients Number:?'
+
+// for dogs:
+// show another different messsage..
+// different summary such as total dog number
+
+
+function orderFood(foodName,numFood){
+    
+    return `Ordering ${numFood} ${foodName}s..ready in 2 mins`;  
+  
+}
+
+console.log(orderFood('Tea',2));
+
+console.log(orderFood('Ice Cream',10));
+
+
+console.log(orderFood('Pizza'));
+console.log(orderFood('Pizza'));
+console.log(orderFood('Pizza'));
+
+
+
+
+
+
+
+
